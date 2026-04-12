ee68d6146e61279fed477e34b22cf3a8dbb1f638
Areeba Kalwar <reebayk05@gmail.com>
2026-04-13 01:50:36 +0500

    fix: rm some varrs

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,14 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdOe1reWK4chEBuc5OgbM5RFk_0Hrw_jg",
-  authDomain: "saanjh-a38e4.firebaseapp.com",
-  projectId: "saanjh-a38e4",
-  storageBucket: "saanjh-a38e4.firebasestorage.app",
-  messagingSenderId: "169562926024",
-  appId: "1:169562926024:web:c7ea8d2e7f7b3ad0bfaefd",
-  measurementId: "G-3XPQ5VR334"
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
